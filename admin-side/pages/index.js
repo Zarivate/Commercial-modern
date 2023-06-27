3b6fae922a30381c28045767239406d769c48748
Zarivate <Zarivate1@gmail.com>
2023-06-26 23:59:22 -0500

    Finally got Google Auth to work, that was a pain

--- admin-side/pages/index.js
+++ admin-side/pages/index.js
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <div className={"bg-red-300"}>Howdy</div>;
+import { useSession, signIn, signOut } from "next-auth/react";
+export default function Component() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  );
 }
