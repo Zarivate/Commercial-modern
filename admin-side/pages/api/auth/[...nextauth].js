9917a7b21bd3d8dfa814a51e0923b78a5ac95c51
Zarivate <Zarivate1@gmail.com>
2023-06-27 03:18:01 -0500

    Connected to database, working on admin portal functions

--- admin-side/pages/api/auth/[...nextauth].js
+++ admin-side/pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -9,6 +11,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+  adapter: MongoDBAdapter(clientPromise),
 };
 
 export default NextAuth(authOptions);
--- admin-side/pages/index.js
+++ admin-side/pages/index.js
@@ -1,18 +1,28 @@
+import Nav from "@/components/Nav";
 import { useSession, signIn, signOut } from "next-auth/react";
 export default function Component() {
   const { data: session } = useSession();
-  if (session) {
+  if (!session) {
     return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
+      <div className="bg-blue-900 w-screen h-screen flex  items-center">
+        <div className="text-center w-full">
+          <button
+            onClick={() => signIn("google")}
+            className="bg-white p-2 px-4 rounded-lg"
+          >
+            Sign in with Google
+          </button>
+        </div>
+      </div>
     );
   }
   return (
     <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <div className="bg-blue-900 min-h-screen">
+        <Nav />
+        Signed in as {session.user.email}
+        <button onClick={() => signOut()}>Sign out</button>
+      </div>
     </>
   );
 }
