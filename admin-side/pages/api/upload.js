c24ba57912de76fa2fd0062e6d88b35c56418c69
Zarivate <Zarivate1@gmail.com>
2023-07-11 23:57:59 -0500

    Fixed image error, was missing await for upload

--- admin-side/pages/api/upload.js
+++ admin-side/pages/api/upload.js
@@ -10,45 +10,51 @@ const bucketName = "nextjs-ecommerce-test";
 export default async function handle(req, res) {
   const form = new multiparty.Form();
 
-  form.parse(req, (err, fields, files) => {
-    console.log("length", files.file.length);
-    const client = new S3Client({
-      region: "us-east-1",
-      credentials: {
-        accessKeyId: process.env.S3_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_KEY,
-      },
+  const { fields, files } = await new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) reject(err);
+      resolve({ fields, files });
     });
-    // Array that will hold the possible many links to the images uploaded
-    const links = [];
-
-    // For loop to add each file to the backend bucket
-    for (const file of files.file) {
-      // Grab the extension for the file name by getting the property value from it, splitting it at the period, then popping the top element
-      const ext = file.originalFilename.split(".").pop();
-
-      // TODO: Make this a more secure and unique key maybe
-      const newFileName = Date.now() + "" + ext;
-
-      console.log({ ext, file });
-      client.send(
-        new PutObjectCommand({
-          Bucket: bucketName,
-          // In order to avoid any files having the same key, a randomly unique generated key is created using the name of the file
-          Key: newFileName,
-          Body: fs.readFileSync(file.path),
-          ACL: "public-read",
-          ContentType: mime.lookup(file.path),
-        })
-      );
-      // Get the unique link for the image on the backend bucket
-      const link = `https://${bucketName}.s3.amazonaws.com/${newFileName}`;
-      // Add the link to the links array
-      links.push(link);
-    }
-
-    res.json({ links });
   });
+
+  // console.log("length", files.file.length);
+  const client = new S3Client({
+    region: "us-east-1",
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
+    },
+  });
+
+  // Array that will hold the possible many links to the images uploaded
+  const links = [];
+
+  // For loop to add each file to the backend bucket
+  for (const file of files.file) {
+    // Grab the extension for the file name by getting the property value from it, splitting it at the period, then popping the top element
+    const ext = file.originalFilename.split(".").pop();
+
+    // TODO: Make this a more secure and unique key maybe
+    const newFileName = Date.now() + "." + ext;
+
+    console.log({ ext, file });
+    await client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        // In order to avoid any files having the same key, a randomly unique generated key is created using the name of the file
+        Key: newFileName,
+        Body: fs.readFileSync(file.path),
+        ACL: "public-read",
+        ContentType: mime.lookup(file.path),
+      })
+    );
+    // Get the unique link for the image on the backend bucket
+    const link = `https://${bucketName}.s3.amazonaws.com/${newFileName}`;
+    // Add the link to the links array
+    links.push(link);
+  }
+
+  return res.json({ links });
 }
 
 // This is to tell NextJS to not parse the data into JSON since we want to parse it ourselves. Because of this all the data
