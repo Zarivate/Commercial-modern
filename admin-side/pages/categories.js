246b4b97190d954cf558ffd80eedbc856eecf234
Zarivate <Zarivate1@gmail.com>
2023-07-15 23:03:52 -0500

    Added logout feature and new backend document model

--- admin-side/pages/categories.js
+++ admin-side/pages/categories.js
@@ -1,16 +1,39 @@
 import Layout from "@/components/Layout";
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
-function categories() {
+export default function Categories() {
+  const [name, setName] = useState("");
+
+  async function saveInfo(e) {
+    e.preventDefault();
+    await axios.post("/api/categories", { name });
+    setName("");
+  }
+
   return (
     <Layout>
       <h1>Categories</h1>
-      <label>New Category Name</label>
-      <form onSubmit={console.log("yes")} className="flex gap-1">
-        <input className="mb-0" type="text" />
+      <label>New category name</label>
+      <form onSubmit={saveInfo} className="flex gap-1">
+        <input
+          className="mb-0"
+          type="text"
+          placeholder={"Example name"}
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <button type="submit" className="btn-primary py-1">
+          Save
+        </button>
       </form>
+      <table className="basic mt-4">
+        <thead>
+          <tr>
+            <td>Category Name</td>
+          </tr>
+        </thead>
+      </table>
     </Layout>
   );
 }
-
-export default categories;
