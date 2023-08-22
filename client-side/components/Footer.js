d4880579fed031b149f41e3726e97844d7747a57
Zarivate <Zarivate1@gmail.com>
2023-08-21 23:59:05 -0500

    Footer works fine now, cart doesn't empty after order completion however

--- client-side/components/Footer.js
+++ client-side/components/Footer.js
@@ -22,25 +22,65 @@ const Footer = () => {
           <FooterLinksWrapper>
             <FooterLinkItems>
               <FooterLinkTitle>About Us </FooterLinkTitle>
-              <FooterLink to="/">How it works</FooterLink>
-              <FooterLink to="/">Testimonials</FooterLink>
-              <FooterLink to="/">Careers</FooterLink>
-              <FooterLink to="/">Investors</FooterLink>
-              <FooterLink to="/">Terms of Service</FooterLink>
+              <FooterLink href={"/"}>How it works</FooterLink>
+              <FooterLink href={"/"}>Testimonials</FooterLink>
+              <FooterLink href={"/"}>Careers</FooterLink>
+              <FooterLink href={"/"}>Investors</FooterLink>
+              <FooterLink href={"/"}>Terms of Service</FooterLink>
             </FooterLinkItems>
             <FooterLinkItems>
-              <div>Hello</div>
+              <FooterLinkTitle>Contact Us</FooterLinkTitle>
+              <FooterLink href={"/"}>Customer Support</FooterLink>
+              <FooterLink href={"/"}>Email</FooterLink>
+              <FooterLink href={"/"}>P.O Box</FooterLink>
+              <FooterLink href={"/"}>Fax Machine</FooterLink>
             </FooterLinkItems>
           </FooterLinksWrapper>
           <FooterLinksWrapper>
             <FooterLinkItems>
-              <div>Hello</div>
+              <FooterLinkTitle>Employees</FooterLinkTitle>
+              <FooterLink href={"/"}>Janitors</FooterLink>
+              <FooterLink href={"/"}>Ambassadors</FooterLink>
+              <FooterLink href={"/"}>Agency</FooterLink>
+              <FooterLink href={"/"}>Influencer</FooterLink>
             </FooterLinkItems>
             <FooterLinkItems>
-              <div>Hello</div>
+              <FooterLinkTitle>Social Media</FooterLinkTitle>
+              <FooterLink href={"/"}>Facebook</FooterLink>
+              <FooterLink href={"/"}>Instagram</FooterLink>
+              <FooterLink href={"/"}>Youtube</FooterLink>
+              <FooterLink href={"/"}>Twitter</FooterLink>
+              <FooterLink href={"/"}>LinkedIn</FooterLink>
             </FooterLinkItems>
           </FooterLinksWrapper>
         </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo href={"/"} onClick={toggleHome}>
+              Commerce
+            </SocialLogo>
+            <WebsiteRights>
+              Commerce © {new Date().getFullYear()} All rights reserved.
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href={"/"} target="_blank" aria-label="Facebook">
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href={"/"} target="_blank" aria-label="Instagram">
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href={"/"} target="_blank" aria-label="Youtube">
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink href={"/"} target="_blank" aria-label="Twitter">
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href={"/"} target="_blank" aria-label="Linkedin">
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
       </FooterWrap>
     </FooterContainer>
   );
